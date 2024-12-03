beb82d2fba42ae06d99da8a3e5273eade1a57fbb
FelpsDias <153229101+FelpsDias@users.noreply.github.com>
2024-12-03 12:21:41 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,14 @@
 <?php 
-
 $MYSQLHOST = 'junction.proxy.rlwy.net';
 $MYSQLUSER = 'root';
 $MYSQL_ROOT_PASSWORD = 'VPRPnXtxIuPJQdsjUlkbOmZDJmuSlvLl';
 $MYSQL_DATABASE = 'railway';
+$MYSQL_PORT = 33899;
 
-$conexao = new mysqli($MYSQLHOST, $MYSQLUSER, $MYSQL_ROOT_PASSWORD, $MYSQL_DATABASE);
+// Criar a conexão
+$conexao = new mysqli($MYSQLHOST, $MYSQLUSER, $MYSQL_ROOT_PASSWORD, $MYSQL_DATABASE, $MYSQL_PORT);
 
+// Verificar a conexão
 if($conexao->connect_errno){
     echo "Erro na conexão: " . $conexao->connect_error;
 }else{
